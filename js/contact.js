fedaea2e1ff197f526cb54cc0d5a1aa166a2106a
Julio Lucas <julio.lucas@gmail.com>
2020-01-07 23:45:41 +0000

    ver 1

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+    (function ($) {
+        "use strict";
+
+
+        jQuery.validator.addMethod('answercheck', function (value, element) {
+            return this.optional(element) || /^\bcat\b$/.test(value)
+        }, "type the correct answer -_-");
+
+        // validate contactForm form
+        $(function () {
+            $('#contactForm').validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    subject: {
+                        required: true,
+                        minlength: 4
+                    },
+                    number: {
+                        required: true,
+                        minlength: 5
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message: {
+                        required: true,
+                        minlength: 20
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Indique o seu nome",
+                        minlength: "O nome deverá conter pelo menos dois caracteres"
+                    },
+                    subject: {
+                        required: "Indique o assunto",
+                        minlength: "O assunto deverá conter pelo menos quatro caracteres"
+                    },
+                    number: {
+                        required: "come on, you have a number, don't you?",
+                        minlength: "your Number must consist of at least 5 characters"
+                    },
+                    email: {
+                        required: "Indique o seu email"
+                    },
+                    message: {
+                        required: "Escreva uma mensagem",
+                        minlength: "A mensagem deverá conter pelo menos vinte caracteres"
+                    }
+                },
+                submitHandler: function (form) {
+                    $(form).ajaxSubmit({
+                        type: "POST",
+                        data: $(form).serialize(),
+                        url: "contact_process.php",
+                        success: function () {
+                            $('#contactForm :input').attr('disabled', 'disabled');
+                            $('#contactForm').fadeTo("slow", 1, function () {
+                                $(this).find(':input').attr('disabled', 'disabled');
+                                $(this).find('label').css('cursor', 'default');
+                                $('#success').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#success').modal('show');
+                            })
+                        },
+                        error: function () {
+                            $('#contactForm').fadeTo("slow", 1, function () {
+                                $('#error').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#error').modal('show');
+                            })
+                        }
+                    })
+                }
+            })
+        })
+
+    })(jQuery)
+})
